701e8aba02b32b967643bda32d403544040fb8ba
Philip Demand <philipdemand@yahoo.com>
2024-05-24 23:18:52 -0400

    scroll button styling

--- src/ScrollButton.js
+++ src/ScrollButton.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ScrollButton({ scrollTop, lightClicked }) {
   return (
-    <div className="right-fixed-component">
+    <div className="right-fixed-component" style={{backgroundColor: lightClicked ? "#235b6e" : "#ddeef4", borderRadius: "5px" }}>
         <FontAwesomeIcon className={!lightClicked ? "icon-light" : "icon-dark"} icon={faArrowUp} onClick={scrollTop}/>
     </div>
   );
