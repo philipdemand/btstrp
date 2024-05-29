dbc8a40c9e463b4fc1b785b3a3f08d36baa94481
Philip Demand <philipdemand@yahoo.com>
2024-05-29 11:53:27 -0400

    form color, project techs

--- src/misc/Projects.js
+++ src/misc/Projects.js
@@ -2,7 +2,7 @@ export const PROJECTS = [
     {
       title: "Gig-Board - A Gig-Finder for Musicians",
       description: "An app where musicians can find and apply for gigs, and where holders of gigs can find musicians.  Frontend built with React.  Backend build with Ruby On Rails using a PostgreSQL database, bcrypt for password-hashing, and delegated types to distinguish user-types: musicians and directors. Styled using React Bootstrap",
-      technologies: ["nodejs", "npm", "chrome", "ruby", "rails", "react", "reactrouter", "reactbootstrap", "css3", "git", "github", "postman", "html5", "postgresql", "homebrew", "vscode"],
+      technologies: ["nodejs", "javascript", "npm", "chrome", "ruby", "rails", "react", "reactrouter", "reactbootstrap", "css3", "git", "github", "postman", "html5", "postgresql", "homebrew", "vscode"],
       page: "https://gig-board.onrender.com",
       github: "https://github.com/philipdemand/gig-board-app",
       alt: "gig-board",
@@ -15,7 +15,7 @@ export const PROJECTS = [
     {
       title: "E-Vent - An Event-Planning App",
       description: "An app where users can announce events, and where users can register and manage their attendance at events.  Frontend built with React utilizing React-Router.  Backend build with Ruby On Rails using bcrypt for password-hashing.",
-      technologies: ["npm", "ruby", "rails", "react", "reactrouter", "chrome", "css3", "git", "postman", "nodejs", "github", "html5", "mysql", "homebrew", "vscode"],
+      technologies: ["npm", "ruby", "rails", "react", "reactrouter", "chrome", "css3", "git", "postman", "nodejs", "javascript", "github", "html5", "mysql", "homebrew", "vscode"],
       page: null,
       github: "https://github.com/philipdemand/e-vent-app",
       alt: "e-vent",
